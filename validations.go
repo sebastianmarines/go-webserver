038dee74dc633623832037469803d180999f8436
Sebastian Marines <sebastian0marines@gmail.com>
2022-12-16 23:29:48 -0600

    Validate HTTP verb on client request

--- validations.go
+++ validations.go
@@ -5,9 +5,20 @@ import (
 )
 
 func validateHttpStartLine(line string) (method, path, proto string, err error) {
+	supportedVerbs := []string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"}
 	_, err = fmt.Sscanf(line, "%s %s %s", &method, &path, &proto)
 	if err != nil {
 		err = fmt.Errorf("invalid http start line: %s", line)
+		return "", "", "", err
+	}
+	validVerb := false
+	for _, m := range supportedVerbs {
+		if m == method {
+			validVerb = true
+		}
+	}
+	if !validVerb {
+		err = fmt.Errorf("invalid http verb: %s", method)
 		return "", "", "", err
 	}
 	err = nil
